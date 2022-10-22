4d94173d0828f3f49da87782b2f9e0973962c2ef
Pankit Thakkar <pankitthakkar007@gmail.com>
2022-10-22 18:48:07 -0400

    Pankit's favorite lunch

--- src/main/java/ATM.java
+++ src/main/java/ATM.java
@@ -23,6 +23,7 @@ public class ATM {
     private static final int WITHDRAWAL = 2;
     private static final int DEPOSIT = 3;
     private static final int EXIT = 4;
+    private static final int PANKIT = 5;
     private static final int FAZEEL = 6;
     private static final int DHYEY = 7;
 
@@ -89,11 +90,15 @@ public class ATM {
                     currentTransaction = createTransaction(mainMenuSelection);
                     currentTransaction.execute(); // execute transaction
                     break;
-		case FAZEEL:
-                    screen.displayMessageLine("Sushi");
-		    case DHYEY:
-			  screen.displayMessageLine("Pizza");
-			  break;
+                case PANKIT:
+                    screen.displayMessageLine("\nPav Bhaji");
+                    break;
+		        case FAZEEL:
+                    screen.displayMessageLine("\nSushi");
+		            break;
+                case DHYEY:
+			        screen.displayMessageLine("\nPizza");
+			        break;
                 case EXIT: // user chose to terminate session
                     screen.displayMessageLine("\n[~] Exiting the system...");
                     userExited = true; // this ATM session should end
@@ -111,7 +116,8 @@ public class ATM {
         screen.displayMessageLine("1 - View my balance");
         screen.displayMessageLine("2 - Withdraw cash");
         screen.displayMessageLine("3 - Deposit funds");
-        screen.displayMessageLine("4 - Exit\n");
+        screen.displayMessageLine("4 - Exit");
+        screen.displayMessageLine("5 - Pankit's favourite lunch");
         screen.displayMessageLine("6 - Fazeel's favorite lunch\n");
         screen.displayMessage("[?] Enter a choice: ");
         return keypad.getInput(); // return user's selection
